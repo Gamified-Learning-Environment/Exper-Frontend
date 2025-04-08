d164d401be94d0e5624edf8692f72b5eeaa081cc
Shane Walsh <swalsh818@gmail.com>
2025-04-08 15:37:13 +0100

    Attempting to fix static file generation issues

--- experfrontend/next.config.ts
+++ experfrontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  // Disable static generation of internal pages
+  skipTrailingSlashRedirect: true,
+
   webpack: (config) => {
     // Skip tree-shaking for problematic modules
     config.optimization.usedExports = false;
